15c76ab651b13d6fba523fef4cc65537f4e10d33
fiscn <fiscn2709@wrdsb.ca>
2020-10-06 13:28:42 +0000

    Finished A5 Q2 program

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner; 
+
 /**
- *
- * @author 
+ * program that will have the user enter in a positive integer. It will then print the numbers from 1 to that number on a single line with commas in between them. 
+ * @author Neil Fischer 
  */
 public class Main {
 
@@ -9,7 +11,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create scanner for user import 
+    Scanner input = new Scanner(System.in);  
+
+    //prompt the user to enter a positive integer 
+    System.out.println("Please enter a positive integer"); 
+
+    //declare variable for user input 
+    int userNM = input.nextInt(); 
+
+    //create loop for number print out 
+    for(int count = 1; count < userNM; count++) 
+    //print out numbers to screen 
+    System.out.print(count + " ,"); 
+    System.out.print(" " + userNM); 
+     
+   
+
+
+  
     
   }
 }
